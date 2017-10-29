f6af6aa26b44fe8ff7f348838d3bd384de5da43d
Ajla Bock <32345037+ajlabock@users.noreply.github.com>
2017-10-29 08:32:33 -0600

    Create more_fish.js

---
+++ _posts/more_fish.js
@@ -0,0 +1,21 @@
+background(186, 145, 20); // wooden table
+fill(96, 161, 240);
+ellipse(200, 200, 350, 350); // plate
+fill(235, 226, 226);
+ellipse(200, 200, 300, 300); 
+
+// Chicken leg
+fill(255, 255, 255);
+stroke(148, 37, 37);
+strokeWeight(4);
+rotate(45);
+rect(171, -12, 100, 23, 20);
+
+fill(236, 245, 166);
+stroke(0, 0, 0);
+rotate(-45);
+ellipse(100, 100, 100, 100);
+arc(215,269,605,93,73,111);
+
+fill(207, 227, 204);
+bezier(229,103,503,276,404,-23,247,175);
